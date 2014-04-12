03300855152c23120c7c6f58ddd4f42e30b9c2f1
Vincent REY <vrey@e2r11p19.42.fr>
2014-04-12 21:41:20 +0200

    SQL queries working

--- attlestar/php/SQLdata.Class.php
+++ attlestar/php/SQLdata.Class.php
@@ -4,7 +4,7 @@ Class SQLdata
 
 	public function creatUnivers($data)
 	{
-		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "qwertyus");
+		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "abc123");
 		$query = $db->prepare("INSERT INTO game (data) VALUES (?)");
 		$query->execute(array(base64_encode(serialize($data))));
 		return $db->lastInsertId();
@@ -13,9 +13,9 @@ Class SQLdata
 
 	public function getUnivers($id_game)
 	{
-		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "qwertyus");
+		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "abc123");
 		$query = $db->prepare("SELECT data FROM game WHERE ? = id");
-		$query->execute(array(5));
+		$query->execute(array($id_game));
 		$data = $query->fetch();
 		return unserialize(base64_decode($data['data']));
 		$db->close();
@@ -23,16 +23,16 @@ Class SQLdata
 
 	public function setUnivers($id_game, $data)
 	{
-		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "qwertyus");
+		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "abc123");
 		$query = $db->prepare("UPDATE game SET data=':data' WHERE :id_game = game.id");
 		$query->execute(array(':data' => serialize($data),
-						':id_game' => $id_game));
+							':id_game' => $id_game));
 		$db->close();
 	}
 
 	public function getPlayer($id_game)
 	{
-		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "qwertyus");
+		$db = new PDO("mysql:host=127.0.0.1;dbname=rush2", "root", "abc123");
 		$db->prepare("SELECT id, name FROM player game WHERE ? = game.id AND game.id = player.id_game");
 		foreach ($db->exec($id_game) as $value)
 			$data[] = $value;
