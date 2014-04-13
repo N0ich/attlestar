1952a20267502fba06a8e6858cb0509c0611bedb
Vincent REY <vrey@e2r11p19.42.fr>
2014-04-13 21:34:16 +0200

    missing doc

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 	function	connect() {
 		try {
-			$db = new PDO('mysql:host=localhost;dbname=rush02', "root", "eeghu9lu");
+			$db = new PDO('mysql:host=localhost;dbname=rush02', "root", "abc123");
 		} catch (PDOException $e) {
 			print "Erreur !: " . $e->getMessage() . "<br/>";
 			die();
